ebcb6f18a56917f9b3f3f9dbb596d89dbc4500e8
Byrix <brosea101@gmail.com>
2023-10-16 18:14:13 +1100

    cog quote update

--- cogs/quote.py
+++ cogs/quote.py
@@ -1,6 +1,7 @@
 import disnake
 from disnake.ext import commands
 import mariadb
+import re
 
 
 class Quote(commands.Cog):
@@ -22,29 +23,35 @@ class Quote(commands.Cog):
         await inter.response.defer()
         if query is not None and query.isdigit():
             # Get ref
-            print('Ref')
             self.db.execute(f'SELECT * FROM quotes ORDER BY id LIMIT 1 OFFSET {int(query)-1}')
         elif type(query) is str:
             # Search
-            print('search')
             self.db.execute(f"SELECT * FROM quotes WHERE quote LIKE '%{query}%' ORDER BY RAND() LIMIT 1")
         else:
             # Get random
-            print('random')
-            print(type(query))
-            print(query)
             self.db.execute('SELECT * FROM quotes ORDER BY RAND() LIMIT 1')
 
+        quote = None
         for obj in self.db:
             quote = obj
             break
 
+        if quote is None:
+            await inter.edit_original_response(content='No quote found!')
+            return
+
+        quote_split = quote[1].split("\"")
         quoteEmbed = disnake.Embed(
-            title=quote[1],
-            description=quote[3],
+            title=quote_split[1],
             timestamp=quote[4],
             colour=disnake.Colour.from_rgb(124, 4, 244)
         )
+        quoteEmbed.add_field(name='Game', value=quote[3], inline=True)
+
+        if quote_split[2] != "":
+            quote_author = re.sub('[^A-z]', '', quote_split[2])
+            quoteEmbed.add_field(name='Said By', value=quote_author, inline=True)
+
         await inter.edit_original_response(embed=quoteEmbed)
 
 def setup(bot):
